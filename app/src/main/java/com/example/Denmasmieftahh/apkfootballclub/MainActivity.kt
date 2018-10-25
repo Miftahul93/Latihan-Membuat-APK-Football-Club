f39b35172caca1d2d3010a47ea4be9f0ec22a8d3
Miftahul93 <miftahululum693@gmail.com>
2018-10-25 23:45:33 +0700

    Mendesain layout

--- app/src/main/java/com/example/Denmasmieftahh/apkfootballclub/MainActivity.kt
+++ app/src/main/java/com/example/Denmasmieftahh/apkfootballclub/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.Denmasmieftahh.apkfootballclub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import com.example.Denmasmieftahh.apkfootballclub.R.color.colorAccent
+import kotlinx.android.synthetic.main.abc_search_dropdown_item_icons_2line.view.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
+    private lateinit var language: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            spinner = spinner ()
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light)
+
+                relativeLayout{
+                    lparams(width = matchParent, height = wrapContent)
+
+                    listTeam = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
+
+                    progressBar = progressBar {
+
+                    }.lparams {
+                        centerHorizontally()
+                    }
+                }
+            }
+        }
     }
 }
